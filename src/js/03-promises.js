ab215e37e792973df5c720357049bd9d13f14dbf
andramaria30 <146965138+andramaria30@users.noreply.github.com>
2024-09-04 23:52:23 +0300

    Add files via upload

---
+++ src/js/03-promises.js
@@ -0,0 +1,49 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const delay = parseInt(form.elements.delay.value);
+  const step = parseInt(form.elements.step.value);
+  const amount = parseInt(form.elements.amount.value);
+
+  if (delay <= 0 || step < 0 || amount < 0) {
+    return Notiflix.Report.warning(
+      'Opsss....🧟‍♂️',
+      'The number must be greater than 0',
+      'Try again'
+    );
+  }
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(i, delay + step * i)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+});
+
+function createPromise(position, delay) {
+  return new Promise((res, rej) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        res({ position, delay });
+      } else {
+        // Reject
+        rej({ position, delay });
+      }
+    }, delay);
+  });
+}
+
